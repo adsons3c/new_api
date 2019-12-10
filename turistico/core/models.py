29a219dd07a726d5739e5a8daeaae75243542e6b
adsons3c <adsonemanuels3c@gmail.com>
2019-12-10 06:52:59 -0300

    add mais dois apps

--- turistico/core/models.py
+++ turistico/core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from recursos.models import Recursos
+from comentarios.models import Comentarios
+
 
 class PontoTurisco(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
     recurso = models.ManyToManyField(Recursos)
+    comentarios = models.ManyToManyField(Comentarios)
 
     def __str__(self):
         return self.nome
